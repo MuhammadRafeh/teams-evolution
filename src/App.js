b3dc44fde764739ee9789c0e2314c734a82a31cb
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-08-29 11:18:24 +0500

    Designed first screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 
-import logo from './assets/logo.svg';
+import Front from './components/Front';
 
 function App() {
   return (
     <div className="App">
 
-      <div>
-        <img src={logo} alt={'unable to load'} />
-      </div>
+
+      <Front />
 
     </div>
   );
